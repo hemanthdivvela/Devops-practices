90d3d495ee965f0359bb9b9fe9138508b8cf4793
hemanthdivvela <divvelahemanth7@gmail.com>
2024-06-19 17:29:17 +0530

    commit

---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username::"
+
+read USERNAME #HERE USERNAME IS variable
+
+echo "please enter Password::"
+
+read PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
